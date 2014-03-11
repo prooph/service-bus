3408eee280a03886fac2b0d295a203d8a34ba5a6
Codeliner <kontakt@codeliner.ws>
2014-03-11 21:28:53 +0100

    Update #5: Add AbstractEvent and EventInterface

---
+++ tests/Codeliner/ServiceBusTest/Mock/SomethingDone.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is part of the codeliner/php-service-bus.
+ * (c) Alexander Miertsch <kontakt@codeliner.ws>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 11.03.14 - 21:17
+ */
+
+namespace Codeliner\ServiceBusTest\Mock;
+
+use Codeliner\ServiceBus\Event\AbstractEvent;
+
+/**
+ * Class SomethingDone
+ *
+ * @package Codeliner\ServiceBusTest\Mock
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class SomethingDone extends AbstractEvent
+{
+}
+ 
