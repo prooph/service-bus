8dc30d5c521f57cb01e6bdbb7f56e13eecbe914a
Codeliner <kontakt@codeliner.ws>
2014-09-14 18:11:31 +0200

    Add new CommandBus class

---
+++ src/Prooph/ServiceBus/CommandBus.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the prooph/service-bus.
+ * (c) Alexander Miertsch <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * 
+ * Date: 14.09.14 - 16:32
+ */
+
+namespace Prooph\ServiceBus;
+
+/**
+ * Class CommandBus
+ *
+ * @package Prooph\ServiceBus
+ * @author Alexander Miertsch <kontakt@codeliner.ws>
+ */
+class CommandBus 
+{
+
+}
+ 
