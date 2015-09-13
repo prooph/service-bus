53d25eed1f66adaeacaf0077341d5e23dc00a214
prolic <saschaprolic@googlemail.com>
2015-09-13 19:47:52 +0100

    add licenses to php file headers

--- tests/Container/Plugin/Guard/RouteGuardFactoryTest.php
+++ tests/Container/Plugin/Guard/RouteGuardFactoryTest.php
@@ -1,4 +1,13 @@
 <?php
+/*
+ * This file is part of the prooph/service-bus.
+ * (c) 2014-2015 prooph software GmbH <contact@prooph.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Date: 09/13/15 -7:47 PM
+ */
 
 namespace Prooph\ServiceBusTest\Container\Plugin\Guard;
 
