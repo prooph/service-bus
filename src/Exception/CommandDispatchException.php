867692887686ef31bcd4f4de1638f6c5b07e7b34
Jefersson Nathan <admin@phpse.net>
2016-08-30 22:29:51 -0300

    Normalize all license headers

--- src/Exception/CommandDispatchException.php
+++ src/Exception/CommandDispatchException.php
@@ -1,13 +1,12 @@
 <?php
 /*
  * This file is part of the prooph/service-bus.
- * (c) 2014-2015 prooph software GmbH <contact@prooph.de>
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * Date: 11/21/15 - 6:30 PM
  */
+
 namespace Prooph\ServiceBus\Exception;
 
 /**
--- src/Plugin/Router/RegexRouter.php
+++ src/Plugin/Router/RegexRouter.php
@@ -1,12 +1,10 @@
 <?php
 /*
  * This file is part of the prooph/service-bus.
- * (c) 2014 - 2015 prooph software GmbH <contact@prooph.de>
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * Date: 10/30/14 - 22:29
  */
 
 namespace Prooph\ServiceBus\Plugin\Router;
