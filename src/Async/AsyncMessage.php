84b4550e41abf6e7aa4cf6d0f3ed9dc2ee607a32
prolic <saschaprolic@googlemail.com>
2016-09-02 18:01:49 +1200

    Revise files header license

--- src/Async/AsyncMessage.php
+++ src/Async/AsyncMessage.php
@@ -1,12 +1,11 @@
 <?php
-/*
+/**
  * This file is part of the prooph/service-bus.
  * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * Date: 2016-08-11T08:16:00Z
  */
 namespace Prooph\ServiceBus\Async;
 
--- tests/Plugin/Router/EventRouterTest.php
+++ tests/Plugin/Router/EventRouterTest.php
@@ -1,12 +1,11 @@
 <?php
-/*
+/**
  * This file is part of the prooph/service-bus.
- * (c) Alexander Miertsch <contact@prooph.de>
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- *
- * Date: 09/23/14 - 20:37
  */
 
 namespace ProophTest\ServiceBus\Plugin\Router;
