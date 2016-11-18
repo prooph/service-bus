f0e9f69e28ea2aa72f96eb7171b5c8246882ba73
prolic <saschaprolic@googlemail.com>
2016-11-18 17:10:12 +0800

    add ServiceLocatorEventRouter & SingleHandlerServiceLocatorRouter, resolves: https://github.com/prooph/service-bus/issues/106

---
+++ tests/Plugin/Router/ServiceLocatorEventRouterTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of the prooph/service-bus.
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\ServiceBus\Plugin\Router;
+
+use Interop\Container\ContainerInterface;
+use Prooph\Common\Event\DefaultActionEvent;
+use Prooph\ServiceBus\EventBus;
+use Prooph\ServiceBus\MessageBus;
+use Prooph\ServiceBus\Plugin\Router\ServiceLocatorEventRouter;
+use ProophTest\ServiceBus\Mock\MessageHandler;
+use ProophTest\ServiceBus\TestCase;
+
+class ServiceLocatorEventRouterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_routes()
+    {
+        $container = $this->prophesize(ContainerInterface::class);
+        $container->has('event')->willReturn(true)->shouldBeCalled();
+        $container->get('event')->willReturn(new MessageHandler())->shouldBeCalled();
+
+        $eventBus = new EventBus();
+
+        $actionEvent = new DefaultActionEvent(MessageBus::EVENT_ROUTE, new EventBus(), [
+            EventBus::EVENT_PARAM_MESSAGE_NAME => 'event',
+        ]);
+
+        $router = new ServiceLocatorEventRouter($container->reveal());
+        $eventBus->utilize($router);
+
+        $router->onRouteMessage($actionEvent);
+
+        $this->assertInstanceOf(MessageHandler::class, $actionEvent->getParam(EventBus::EVENT_PARAM_EVENT_LISTENERS));
+    }
+}
---
+++ tests/Plugin/Router/SingleHandlerServiceLocatorRouterTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * This file is part of the prooph/service-bus.
+ * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace ProophTest\ServiceBus\Plugin\Router;
+
+use Interop\Container\ContainerInterface;
+use Prooph\Common\Event\DefaultActionEvent;
+use Prooph\ServiceBus\CommandBus;
+use Prooph\ServiceBus\MessageBus;
+use Prooph\ServiceBus\Plugin\Router\SingleHandlerServiceLocatorRouter;
+use ProophTest\ServiceBus\Mock\MessageHandler;
+use ProophTest\ServiceBus\TestCase;
+
+class SingleHandlerServiceLocatorRouterTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_routes()
+    {
+        $container = $this->prophesize(ContainerInterface::class);
+        $container->has('message')->willReturn(true)->shouldBeCalled();
+        $container->get('message')->willReturn(new MessageHandler())->shouldBeCalled();
+
+        $commandBus = new CommandBus();
+
+        $actionEvent = new DefaultActionEvent(MessageBus::EVENT_ROUTE, $commandBus, [
+            MessageBus::EVENT_PARAM_MESSAGE_NAME => 'message',
+        ]);
+
+        $router = new SingleHandlerServiceLocatorRouter($container->reveal());
+        $commandBus->utilize($router);
+
+        $router->onRouteMessage($actionEvent);
+
+        $this->assertInstanceOf(MessageHandler::class, $actionEvent->getParam(MessageBus::EVENT_PARAM_MESSAGE_HANDLER));
+    }
+}
