af0dae21a3b49146e45cb9a55c558e450f13040e
prolic <saschaprolic@googlemail.com>
2017-02-16 22:12:50 +0800

    it's 2017!!

--- tests/Plugin/MessageProducerPluginTest.php
+++ tests/Plugin/MessageProducerPluginTest.php
@@ -2,8 +2,8 @@
 
 /**
  * This file is part of the prooph/service-bus.
- * (c) 2014-2016 prooph software GmbH <contact@prooph.de>
- * (c) 2015-2016 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
+ * (c) 2013-2017 prooph software GmbH <contact@prooph.de>
+ * (c) 2015-2017 Sascha-Oliver Prolic <saschaprolic@googlemail.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
